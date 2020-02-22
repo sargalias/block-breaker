fe6b3bebb5acb972610449527dd044a78f2f7ea0
Spyros Argalias <sargalias@gmail.com>
2020-02-22 15:45:24 +0000

    feat(Scripts/Ball): Launch ball on primary mouse button click

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,16 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
+    [SerializeField]
+    private float speed = 10;
+
+    [SerializeField]
+    private Vector2 initialDirection;
+
     private Paddle paddle;
-    private Transform transform;
 
     private void Start() {
+        SetDefaults();
         paddle = FindObjectOfType<Paddle>();
+        SetStickToPaddle();
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Launch();
+        }
+    }
+
+    private void SetDefaults() {
+        if (initialDirection.magnitude == 0) {
+            initialDirection = new Vector2(1, 1);
+        }
+    }
+
+    private void SetStickToPaddle() {
         paddle.Moved += OnPaddleMoved;
-        transform = gameObject.GetComponent<Transform>();
     }
 
     private void OnPaddleMoved(Vector2 position) {
         transform.position = new Vector2(position.x, transform.position.y);
+    }
+
+    private void Launch() {
+        paddle.Moved -= OnPaddleMoved;
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.velocity = initialDirection.normalized * speed;
     }
 }
