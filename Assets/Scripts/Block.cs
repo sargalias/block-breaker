b0b86d1e052f025ef2f103ec1e56c6d4ec3b939d
Spyros Argalias <sargalias@gmail.com>
2020-02-22 09:13:08 +0000

    feat(Scripts/BlockManager): Create intermediary for watching blocks

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+    [SerializeField]
+    Sprite[] blockSprites;
+
+    private int currentSpriteIdx = 0;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        gameObject.SendMessageUpwards("AddBlock", null, SendMessageOptions.RequireReceiver);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision) {
+        if (currentSpriteIdx < blockSprites.Length - 1) {
+            TransitionToNextState();
+        }
+        else {
+            CleanupBlock();
+        }
+    }
+
+    private void TransitionToNextState() {
+        currentSpriteIdx += 1;
+        spriteRenderer.sprite = blockSprites[currentSpriteIdx];
+    }
+
+    private void CleanupBlock() {
+        Destroy(gameObject);
+        gameObject.SendMessageUpwards("DestroyBlock", null, SendMessageOptions.RequireReceiver);
+    }
+}
---
+++ Assets/Scripts/BlocksManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocksManager : MonoBehaviour {
+    private int numBlocksAlive = 0;
+
+    public void AddBlock() {
+        numBlocksAlive++;
+        Debug.Log(numBlocksAlive);
+    }
+
+    public void DestroyBlock() {
+        numBlocksAlive--;
+        Debug.Log(numBlocksAlive);
+    }
+}
