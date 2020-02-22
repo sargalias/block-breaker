e20f8b17b968c305b68e375c102aea53e8e75c81
Spyros Argalias <sargalias@gmail.com>
2020-02-22 06:52:35 +0000

    feat(Scripts/BreakableBlock): Add scripts for changing block sprite on hit

---
+++ Assets/Scripts/BreakableBlock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableBlock : MonoBehaviour {
+    [SerializeField]
+    Sprite[] blockSprites;
+
+    private int currentSpriteIdx = 0;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision) {
+        currentSpriteIdx += 1;
+        if (currentSpriteIdx >= blockSprites.Length) {
+            Destroy(gameObject);
+            return;
+        }
+        spriteRenderer.sprite = blockSprites[currentSpriteIdx];
+    }
+}
