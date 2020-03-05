f0028a2da5642b8c41b40c76b359aa7de60c82ca
Spyros Argalias <sargalias@gmail.com>
2020-03-05 16:24:00 +0000

    refactor(Scripts): Organise scripts

--- Assets/Scripts/BoundaryBottom.cs
+++ Assets/Scripts/BoundaryBottom.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoseCollider : MonoBehaviour {
+public class BoundaryBottom : MonoBehaviour {
     [SerializeField]
     SceneLoader sceneLoader;
 
